ef70700fafd6d5dd9c28625611f11a780cfef6a4
05divya05 <dvemired@purdue.edu>
2025-04-01 15:46:36 -0400

    userprofile class

---
+++ src/UserProfile.java
@@ -0,0 +1,90 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class UserProfile implements Users,Serializable {
+    private String username;  //unique name associated with user
+    private ArrayList<String> friends;  //list of friends of user
+    private ArrayList<String> blockedFriends;  //list of blocked friends of user
+    private String email;  //email associated with user
+    private String password;  //password associated with user
+
+    public UserProfile(String username, String email, String password) {
+
+        this.username = username;
+        this.friends = new ArrayList<String>();
+        this.blockedFriends = new ArrayList<String>();
+        this.email = email;
+        this.password = password;
+
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public void setUsername(String newUsername) {
+
+    }
+
+    @Override
+    public ArrayList<String> getFriends() {
+        return null;
+    }
+
+    @Override
+    public void setFriends(ArrayList<String> newFriends) {
+
+    }
+
+    @Override
+    public String getEmail() {
+        return "";
+    }
+
+    @Override
+    public void setEmail(String newEmail) {
+
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public void setPassword(String newPassword) {
+
+    }
+
+    @Override
+    public boolean addFriend(String userToAdd) {
+        return false;
+    }
+
+    @Override
+    public void removeFriend(String userToRemove) {
+
+    }
+
+    @Override
+    public void blockUser(String userToBlock) {
+
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "";
+    }
+
+    @Override
+    public void saveToFile() {
+
+    }
+
+    @Override
+    public ArrayList<ItemPosts> getUserPosts() {
+        return null;
+    }
+}
