9660b5b6d6325097113a956389a64065ceda179e
05divya05 <99771052+05divya05@users.noreply.github.com>
2025-05-05 13:27:11 -0400

    Create User.java

---
+++ src/User.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+public interface User {
+
+    String getUsername();
+
+    void setUsername(String newUsername);
+
+    ArrayList<String> getFriends();
+
+    void setFriends(ArrayList<String> newFriends);
+
+    String getEmail();
+
+    void setEmail(String newEmail);
+
+    String getPassword();
+
+    void setPassword(String newPassword);
+
+    boolean addFriend(String userToAdd);
+
+    void removeFriend(String userToRemove);
+
+    void blockUser(String userToBlock);
+
+    String toFileFormat();
+
+    void saveToFile();
+
+    ArrayList<NewsPost> getUserPosts();
+
+}
